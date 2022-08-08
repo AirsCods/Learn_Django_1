fa5da6b43b9d20ce3c124e439f4ff217fe3f7da1
AirCods <airscods@gmail.com>
2022-08-08 18:53:36 +0000

    функции View заменены на классы

--- web_site/news/urls.py
+++ web_site/news/urls.py
@@ -18,8 +18,12 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('cat/<int:category_id>', get_category, name='category'),
-    path('<int:news_id>/', view_news, name='view_news'),
-    path('add-news/', add_news, name='add_news'),
+    # path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'),
+    # path('cat/<int:category_id>', get_category, name='category'),
+    path('cat/<int:category_id>', NewsByCategory.as_view(), name='category'),
+    # path('<int:news_id>/', view_news, name='view_news'),
+    path('<int:pk>/', ViewNews.as_view(), name='view_news'),
+    # path('add-news/', add_news, name='add_news'),
+    path('add-news/', CreateNews.as_view(), name='add_news'),
 ]
